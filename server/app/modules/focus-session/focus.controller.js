caa1df1c6753ee6a5a0a32ab0cad2eafdd0f695e
mahabubsaki <md929820@gmail.com>
2025-01-10 05:00:25 +0600

    feat(server) : updated api which includes longest streak also on streak api

--- server/app/modules/focus-session/focus.controller.js
+++ server/app/modules/focus-session/focus.controller.js
@@ -9,10 +9,10 @@ const focusCreateController = catchAsync(async (req, res) => {
 
     const x = await db.focusSession.create({
         data: {
-            completed, paused, duration, userId, timestamp: DateTime.fromISO('2025-01-12T00:00:00.000Z').plus({ hours: 6 }).toJSDate()
+            completed, paused, duration, userId, timestamp: DateTime.now().plus({ hours: 6 }).toJSDate()
         }
     });
-    console.log(x);
+    // console.log(x);
     res.send({ status: true, message: 'focus create controller' });
 });
 
@@ -26,8 +26,8 @@ const convert = (data) => {
     for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
         const formattedDate = d.toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
         const dataForDay = data.filter(item => new Date(item.timestamp).toLocaleDateString('en-US') === d.toLocaleDateString('en-US'));
-        const totalDuration = dataForDay.reduce((sum, item) => sum + Math.round(item.duration / 60), 0);
-        chartData.push({ date: formattedDate, focusMinutes: totalDuration });
+        const totalDuration = dataForDay.reduce((sum, item) => sum + parseFloat(item.duration / 60), 0);
+        chartData.push({ date: formattedDate, focusMinutes: +totalDuration.toFixed(2) });
     }
     return chartData;
 };
@@ -56,13 +56,13 @@ const countStreak = (sessions) => {
         new Date(session.timestamp).toISOString().split('T')[0]
     ))].sort((a, b) => new Date(b) - new Date(a));
     let streakCount = 0;
-    let currentDate = DateTime.fromISO('2025-01-12T00:00:00.000Z').plus({ hours: 6 }).toJSDate();
-    console.log(currentDate, 'currentDate');
+    let currentDate = DateTime.now().plus({ hours: 6 }).toJSDate();
+    // console.log(currentDate, 'currentDate');
 
 
     for (let i = 0; i < dates.length; i++) {
         const date = DateTime.fromISO(dates[i]).plus({ hours: 6 }).toJSDate();
-        console.log(date, 'each-date');
+
         date.setHours(0, 0, 0, 0);
         currentDate.setHours(0, 0, 0, 0);
         if (date.getTime() === currentDate.getTime()) {
@@ -72,6 +72,39 @@ const countStreak = (sessions) => {
     }
 
     return { streakCount };
+};
+
+const countLongestStreak = (sessions) => {
+
+    if (sessions.length === 0) return { longestStreak: 0 };
+
+    const dates = [...new Set(sessions.map(session =>
+        new Date(session.timestamp).toISOString().split('T')[0]
+    ))].sort((a, b) => new Date(a) - new Date(b));
+
+    console.log(dates, 'dates');
+
+    let longestStreak = 1;
+    let currentStreak = 1;
+    let tempStreak = 1;
+
+    for (let i = 0; i < dates.length; i++) {
+        const prevDate = new Date(dates[i]);
+        const currentDate = new Date(dates[i + 1]);
+
+        prevDate.setHours(0, 0, 0, 0);
+        currentDate.setHours(0, 0, 0, 0);
+
+        if ((currentDate.getTime() - prevDate.getTime()) === 24 * 60 * 60 * 1000) {
+            tempStreak++;
+            currentStreak = tempStreak;
+            longestStreak = Math.max(longestStreak, currentStreak);
+        } else {
+            tempStreak = 1;
+        }
+    }
+    console.log(longestStreak, 'longestStreak');
+    return { longestStreak };
 };
 
 const getStreakController = catchAsync(async (req, res) => {
@@ -86,15 +119,118 @@ const getStreakController = catchAsync(async (req, res) => {
             timestamp: 'asc'
         }
     });
-    const result = countStreak(sessions);
 
+    const completedSessions = await db.focusSession.findMany({
+        where: {
+            userId: userId,
+        }
+    });
+    console.log(completedSessions, 'completedSessions');
+    const result = countStreak(sessions);
+    const longestStreak = countLongestStreak(completedSessions);
+    result.sessionNo = completedSessions.length + 1;
+    result.longestStreak = longestStreak.longestStreak;
+
+    // console.log(result, 'result');
+    res.send({ status: true, data: result });
+});
+
+function countCompletedSessions(data) {
+    const endDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - 6);
+
+    const completedSessionsData = [];
+
+    for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+        const formattedDate = d.toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+        const completedSessions = data.filter(
+            item => new Date(item.timestamp).toLocaleDateString('en-US') === d.toLocaleDateString('en-US') && item.completed
+        ).length;
+        completedSessionsData.push({ date: formattedDate, completedSessions: completedSessions });
+    }
+
+    return completedSessionsData;
+}
+
+const getCompletedSessionController = catchAsync(async (req, res) => {
+    const { id: userId } = req.params;
+    console.log(userId, 'id');
+
+    const completedSession = await db.focusSession.findMany({
+        where: {
+            userId: userId,
+            completed: true
+        },
+
+    });
+    const result = countCompletedSessions(completedSession);
+    // console.log(result, 'result');
+    res.send({ status: true, data: result });
+
+
+});
+
+function getHourlyData(data) {
+    const hourlyData = Array(24).fill(0);
+
+    data.forEach(entry => {
+        const hour = new Date(entry.timestamp).getUTCHours();
+        hourlyData[hour] += entry.duration;
+    });
+    hourlyData.forEach((entry, index) => {
+        hourlyData[index] = +parseFloat(entry / 60).toFixed(2);
+    });
+    const result = hourlyData.map((value, index) => {
+        const startHour = index % 24;
+        const endHour = (startHour + 1) % 24;
+        const periodStart = startHour < 12 ? "AM" : "PM";
+        const periodEnd = endHour < 12 ? "AM" : "PM";
+
+
+        const formattedStart = startHour % 12 === 0 ? 12 : startHour % 12;
+        const formattedEnd = endHour % 12 === 0 ? 12 : endHour % 12;
+
+        return {
+            timeRange: `${formattedStart}-${formattedEnd} ${periodStart}`,
+            value: value
+        };
+    });
+
+    return result;
+}
+
+const getDailyFocusSession = catchAsync(async (req, res) => {
+    const { id: userId } = req.params;
+    const time = DateTime.fromISO(req.query.time || new Date().toJSON()).plus({ hours: 6 }).toJSDate();
+    // const start = new Date(time);
+    // start.setHours(0, 0, 0, 0);
+
+    // console.log(DateTime.fromJSDate(time).startOf('day').minus({ day: 1 }).plus({ hours: 6 }).toJSDate(), 'starttime');
+    // console.log(DateTime.fromJSDate(time).endOf('day').minus({ day: 1 }).plus({ hours: 6 }).toJSDate(), 'endtime');
+    console.log(time, 'time', DateTime.fromJSDate(time).startOf('day').toJSDate());
+    const completedSession = await db.focusSession.findMany({
+        where: {
+            userId: userId,
+            timestamp: {
+                gte: DateTime.fromJSDate(time).startOf('day').plus({ hours: 6 }).toJSDate(),
+                lte: DateTime.fromJSDate(time).endOf('day').plus({ hours: 6 }).toJSDate()
+            }
+        },
+
+    });
+    console.log(completedSession, 'completedSession');
+    const result = getHourlyData(completedSession);
     console.log(result, 'result');
     res.send({ status: true, data: result });
+
 });
 
 
 module.exports = {
     focusCreateController,
     getSpecificFocusSession,
-    getStreakController
+    getStreakController,
+    getCompletedSessionController,
+    getDailyFocusSession
 };
--- server/app/modules/focus-session/focus.route.js
+++ server/app/modules/focus-session/focus.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { focusCreateController, getSpecificFocusSession, getStreakController } = require('./focus.controller');
+const { focusCreateController, getSpecificFocusSession, getStreakController, getCompletedSessionController, getDailyFocusSession } = require('./focus.controller');
 const focusRouter = express.Router();
 
 focusRouter.post('/create', focusCreateController);
 focusRouter.get('/get/:id', getSpecificFocusSession);
 focusRouter.get('/streak/:id', getStreakController);
+focusRouter.get('/completed/:id', getCompletedSessionController);
+focusRouter.get('/daily/:id', getDailyFocusSession);
 
 module.exports = focusRouter;
